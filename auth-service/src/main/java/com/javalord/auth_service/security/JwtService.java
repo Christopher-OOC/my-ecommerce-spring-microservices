9f4826fac640e1bc0b1712908ed24378abf9fee7
Christopher-OOC <olojedechristopher24@gmail.com>
2026-03-18 15:18:06 +0100

    commit

--- auth-service/src/main/java/com/javalord/auth_service/security/JwtService.java
+++ auth-service/src/main/java/com/javalord/auth_service/security/JwtService.java
@@ -1,17 +1,20 @@
 package com.javalord.auth_service.security;
 
 import com.javalord.auth_service.dto.AuthResponse;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class JwtService {
@@ -37,8 +40,16 @@ public class JwtService {
             throw new UsernameNotFoundException("Invalid username/password supplied");
         }
         String username = (String)authentication.getPrincipal();
+        Set<String> authorities = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toSet());
 
-        return buildToken(username, Map.of("TOKEN_TYPE", "ACCESS_TOKEN"), accessTokenExpiration);
+        String accessToken = generateAccessToken(username, Map.of("TOKEN_TYPE", "ACCESS_TOKEN", "roles", authorities), accessTokenExpiration);
+        String refreshToken = generateRefreshToken(username, refreshTokenExpiration);
+
+        return new AuthResponse(
+                accessToken,
+                refreshToken,
+                "Bearer"
+        );
     }
 
     public AuthResponse refreshAccessToken() {
@@ -49,6 +60,36 @@ public class JwtService {
         }
         String username = (String)authentication.getPrincipal();
 
-        return buildToken(username, Map.of("TOKEN_TYPE", "REFRESH_TOKEN"), refreshTokenExpiration);
+        String accessToken = generateAccessToken(username, Map.of("TOKEN_TYPE", "ACCESS_TOKEN"), accessTokenExpiration);
+        String refreshToken = generateRefreshToken(username, refreshTokenExpiration);
+
+        return new AuthResponse(
+                accessToken,
+                refreshToken,
+                "Bearer"
+        );
+    }
+
+    private String generateAccessToken(String email, Map<String, Object> claims, long expiration) {
+        return buildToken(email, claims,  expiration);
+    }
+
+    private String generateRefreshToken(String email, long expiration) {
+        return buildToken(email, Map.of("TOKEN_TYPE", "REFRESH_TOKEN"), expiration);
+    }
+
+    private String buildToken(String email, Map<String, Object> claims, Long expiration) {
+        try {
+            return Jwts.builder()
+                    .signWith(this.secretKey)
+                    .claims(claims)
+                    .subject(email)
+                    .issuedAt(new Date())
+                    .expiration(new Date(System.currentTimeMillis() + expiration))
+                    .compact();
+        }
+        catch (JwtException e) {
+            throw new JwtException("Invalid while creating token!");
+        }
     }
 }
