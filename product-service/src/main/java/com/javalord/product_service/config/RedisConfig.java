3968eb133fa4a1dac9904e8ab0f6d7185c8a9760
Christopher-OOC <olojedechristopher24@gmail.com>
2026-03-23 15:00:48 +0100

    commit

--- product-service/src/main/java/com/javalord/product_service/config/RedisConfig.java
+++ product-service/src/main/java/com/javalord/product_service/config/RedisConfig.java
@@ -6,46 +6,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.*;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
 import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisCacheManager cacheManager(RedisConnectionFactory factory) {
-        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(30))
-                .serializeKeysWith(
-                        RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer())
-                )
-                .serializeValuesWith(
-                        RedisSerializationContext.SerializationPair.fromSerializer(new JacksonJsonRedisSerializer<>(ProductResponse.class))
-                );
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig();
+        configuration.entryTtl(Duration.ofHours(1));
+        configuration.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()));
+        configuration.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new JacksonJsonRedisSerializer<>(ProductResponse.class)));
 
-        return RedisCacheManager.builder(factory)
-                .cacheDefaults(config)
-                .build();
+        return RedisCacheManager.builder(connectionFactory).cacheDefaults(configuration).build();
     }
-
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//
-//        redisTemplate.setConnectionFactory(redisConnectionFactory);
-//        redisTemplate.setKeySerializer(new StringRedisSerializer());
-//        redisTemplate.setValueSerializer(new JacksonJsonRedisSerializer<>(ProductResponse.class));
-//
-////        ObjectMapper objectMapper = new ObjectMapper();
-////        objectMapper.registeredModules();
-////
-////        GenericJacksonJsonRedisSerializer serializer =
-////                new GenericJacksonJsonRedisSerializer(objectMapper);
-////
-////
-////        redisTemplate.setValueSerializer(serializer);
-//
-//        return redisTemplate;
-//    }
 
 }
